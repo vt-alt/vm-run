vt-alt/vm-run

commit dd3b3b1bc4e2cc49bc45df2b2d38f681ca02a313

initrd-init: Fix verify-elf warning about executable stack
  verify-elf: WARNING: ./usr/lib/vm-run/initrd-init: found executable
  STACK entry:   GNU_STACK      0x000000 0x0000000000000000
  0x0000000000000000 0x000000 0x000

Makefile
@@ -1,4 +1,4 @@
-CFLAGS += -static
+CFLAGS += -static -Wa,--noexecstack
 initrd-init: initrd-init.c
 clean:
 	-rm -f initrd-init *.o

commit 6b5a549163a89539a5d02e7dfe9f521daea88cac

initrd-init: Fix verify-elf warning about executable stack, part 2
`-Wa,--noexecstack' was not enough.

Fixes: dd3b3b1 ("initrd-init: Fix verify-elf warning about executable stack")

Makefile
@@ -1,4 +1,4 @@
-CFLAGS += -static -Wa,--noexecstack
+CFLAGS += -static -Wl,-z,noexecstack
 initrd-init: initrd-init.c
 clean:
 	-rm -f initrd-init *.o