vt-alt/vm-run

commit 760696dd7b7b371260dd09abe432feba1a4da506

Run shellcheck in make check for all scripts
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -23,6 +23,7 @@ Source: %name-%version.tar
 BuildRequires: klibc-devel
 # For %%check.
 BuildRequires: /dev/kvm
+BuildRequires: shellcheck
 
 # Try to load un-def kernel this way to avoid "forbidden dependencies"
 # from sisyphus_check.
@@ -113,11 +114,7 @@ Run checkinstall tests for vm-run.
 CFLAGS="%optflags" make
 %endif
 
-bash -n vm-run
-bash -n vm-resize
-bash -n vm-run-stub
-bash -n vm-init
-bash -n filetrigger
+make check
 
 %install
 %ifnarch %supported_arches

Makefile
@@ -1,5 +1,19 @@
 CFLAGS += -static -Wl,-z,noexecstack
+scripts = \
+	vm-run \
+	vm-run-stub \
+	vm-init \
+	vm-create-image \
+	vm-resize \
+	filetrigger
+
 initrd-init: initrd-init.c
+
 clean:
 	-rm -f initrd-init *.o
-.PHONY: clean
+
+check: $(scripts)
+	$(foreach f,$^,bash -n $(f);)
+	shellcheck --severity=error $^
+
+.PHONY: clean check

commit 5ac081d046b4adbdd933f7da5c5a7a6903411857

spec: Do not run make check on e2kv4
Fixes: 760696d ("Run shellcheck in make check for all scripts")
Reported-by: Michael Shigorin <mike@altlinux.org>
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -117,9 +117,9 @@ Run checkinstall tests for vm-run.
 %build
 %{?optflags_lto:%global optflags_lto %optflags_lto -ffat-lto-objects}
 CFLAGS="%optflags" make
-%endif
 
 make check
+%endif
 
 %install
 %ifnarch %supported_arches