vt-alt/vm-run

commit 95af423e6360d6322840b1c34969fef38ccf7f63

spec: rpm-build-vm: %post: Remove kernel permission fixing
They shall be fixed in vm-run.filetrigger, so this seems redundant.

Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -167,10 +167,6 @@ install -D -p -m 0755 vm-resize   %buildroot%_bindir/vm-resize
 %_rpmlibdir/vm-run.filetrigger
 %_sysconfdir/bashrc.d/vm*.sh
 
-%post
-# Fix permissions to boot the installed kernel
-find /boot /lib/modules -type f,d \! -perm -444 -print0 | xargs -0r chmod a+rX
-
 %post run
 # Required in case of --udevd option to vm-run
 mkdir -p /run/udev

commit d9751406b4d9b88938196794d71c508c9b107c89

Fix processing of pre-installed kernels
Call filetrigger in `%post run` to simulate its call for the past
kernels. Otherwise they remain unprocessed and vm-run fails in
situation like this:

  $ hsh --ini
  $ hsh-install kernel-image-un-def
  $ hsh-install rpm-build-vm
  $ hsh-shell
  builder@x86_64:/.in$ vm-run
  Error: Cannot find a kernel, try to use '--kernel=' option.
    No kernels found, try to install a kernel first.

Fixes: 95af423 ("spec: rpm-build-vm: %post: Remove kernel permission fixing")
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -190,6 +190,9 @@ chmod a+twx /mnt
 # Allow user creation (for openssh)
 chmod a+r /etc/login.defs
 
+# Call filetrigger for the past kernels.
+find /boot | %_rpmlibdir/vm-run.filetrigger
+
 %pre checkinstall
 PS4=$'\n+ '
 set -ex