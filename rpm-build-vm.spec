vt-alt/vm-run

commit d492e3a1654cb5e4cacea4c2da44d034c6f41f85

spec: no busybox in e2k-alt-linux so far
It's present in OS Elbrus but that build is quite different,
needs more work to adapt.

rpm-build-vm.spec
@@ -163,7 +163,10 @@ install -D -p -m 0755 kvm-ok      %buildroot%_bindir/kvm-ok
 
 %files
 
+%ifnarch %e2k
+# no busybox in e2k-alt-linux so far
 %files checkinstall
+%endif
 
 %files createimage
 %ifarch %supported_arches

commit 2c077c1c7ac38198dac59670363aac278310303a

spec: Remove BR:busybox for all un-supported_arches
Tests are there to solve the problems they uncover, not to disable them.

Fixes: d492e3a ("spec: no busybox in e2k-alt-linux so far")
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -113,7 +113,9 @@ at "/tmp/vm-ext4.img" out of your hasher root to run vm-run with it as rootfs.
 Summary: Checkinstall for vm-run
 Group: Development/Other
 BuildArch: noarch
+%ifarch %supported_arches
 Requires(pre): busybox
+%endif
 Requires(pre): %name-createimage = %EVR
 Requires(pre): procps
 Requires(pre): time
@@ -163,10 +165,7 @@ install -D -p -m 0755 kvm-ok      %buildroot%_bindir/kvm-ok
 
 %files
 
-%ifnarch %e2k
-# no busybox in e2k-alt-linux so far
 %files checkinstall
-%endif
 
 %files createimage
 %ifarch %supported_arches