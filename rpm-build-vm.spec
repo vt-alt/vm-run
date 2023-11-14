vt-alt/vm-run

commit 85a3d99c4cc92bc40762fcd4b1008bf8104ae3eb

spec: checkinstall: Test --initrd move
It should pass exit code correctly.

Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -113,6 +113,7 @@ at "/tmp/vm-ext4.img" out of your hasher root to run vm-run with it as rootfs.
 Summary: Checkinstall for vm-run
 Group: Development/Other
 BuildArch: noarch
+Requires(pre): busybox
 Requires(pre): %name-createimage = %EVR
 Requires(pre): procps
 Requires(pre): time
@@ -217,6 +218,7 @@ timeout 300 vm-run --heredoc <<-EOF
 	uname -a
 	uname -a
 EOF
+timeout 300 vm-run --initrd --append=rddebug 'uname -a; exit 7' || test $? -eq 7
 ! timeout --preserve-status 300 vm-run "true; false; true" || exit 1
 timeout 300 vm-run --mem=max free -g
 timeout 300 vm-run --mem=256 --cpu=max lscpu

commit 33d32530dcf42fd0721ded9263a861810bb9ce6b

spec: checkinstall: Do not run --initrd without busybox
This is theoretical.

Fixes: 85a3d99 ("spec: checkinstall: Test --initrd move")
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

rpm-build-vm.spec
@@ -220,7 +220,9 @@ timeout 300 vm-run --heredoc <<-EOF
 	uname -a
 	uname -a
 EOF
-timeout 300 vm-run --initrd --append=rddebug 'uname -a; exit 7' || test $? -eq 7
+if type -p busybox; then
+	timeout 300 vm-run --initrd --append=rddebug 'uname -a; exit 7' || test $? -eq 7
+fi
 ! timeout --preserve-status 300 vm-run "true; false; true" || exit 1
 timeout 300 vm-run --mem=max free -g
 timeout 300 vm-run --mem=256 --cpu=max lscpu